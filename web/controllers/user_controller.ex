8b18929a73e8d01c351a92129ffb6f5010f0f084
root <root@localhost.localdomain>
2016-10-09 18:03:57 -0300

    more code... users e videos

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,14 +1,36 @@
 defmodule Rumbl.UserController do
-	use Rumbl.Web, :controller
+  use Rumbl.Web, :controller  
+  alias Rumbl.User
 
-	def index(conn, _params) do
-		users = Repo.all(Rumbl.User)
+  plug :authenticate_user when action in [:index, :show]
+
+
+  def index(conn, _params) do
+  	users = Repo.all(Rumbl.User)
 		render conn, "index.html", users: users
-	end
+  end
 
-	def show(conn, %{"id" => id}) do
-		user = Repo.get(Rumbl.User, id)
-		render conn, "show.html", user: user
-	end
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(Rumbl.User, id)
+    render conn, "show.html", user: user
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"user" => user_params}) do
+  	changeset = User.registration_changeset(%User{}, user_params)
+  	case Repo.insert(changeset) do
+  		{:ok, user} ->
+  			conn
+  			|> Rumbl.Auth.login(user)
+  			|> put_flash(:info, "#{user.name} criado!")
+  			|> redirect(to: user_path(conn, :index))
+  		{:error, changeset} ->
+  			render(conn, "new.html", changeset: changeset)
+  	end
+  end
 
 end

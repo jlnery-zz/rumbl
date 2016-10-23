2e0c33a62059143630ec880af4388d763f3626f7
root <root@localhost.localdomain>
2016-10-23 21:14:33 -0200

    Creating channels

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -1,6 +1,8 @@
 defmodule Rumbl.Auth do
   import Plug.Conn
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+  import Phoenix.Controller
+  alias Rumbl.Router.Helpers
 
   def init(opts) do
     Keyword.fetch!(opts, :repo)
@@ -8,15 +10,30 @@ defmodule Rumbl.Auth do
 
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
-    user    = user_id && repo.get(Rumbl.User, user_id)
-    assign(conn, :current_user, user)
+
+    cond do
+      user = conn.assigns[:current_user] ->
+        put_current_user(conn, user) 
+      user = user_id && repo.get(Rumbl.User, user_id) ->
+        put_current_user(conn, user)
+      true ->
+        assign(conn, :current_user, nil)
+    end
   end
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user) 
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
   def logout(conn) do
@@ -37,9 +54,6 @@ defmodule Rumbl.Auth do
         {:error, :not_found, conn}
     end
   end
-
-  import Phoenix.Controller
-  alias Rumbl.Router.Helpers
 
   def authenticate_user(conn, _opts) do
     if conn.assigns.current_user do
